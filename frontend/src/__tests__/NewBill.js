bb846007363cf418fcbfe7a79947b793c3e4a3ef
Florent Marien <Florent62350@hotmail.fr>
2023-09-07 12:43:49 +0200

    Coverage - Newbill

--- frontend/src/__tests__/NewBill.js
+++ frontend/src/__tests__/NewBill.js
@@ -1,18 +1,92 @@
 /**
  * @jest-environment jsdom
  */
-
-import { screen } from "@testing-library/dom"
+import user from '@testing-library/user-event';
+import {fireEvent, screen, wait, waitFor, waitForElement} from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 
+jest.mock("../app/store", () => mockStore)
+let typeok = ["image/png","image/jpg","image/jpeg"];
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    const html = NewBillUI()
+    document.body.innerHTML = html
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname })
+    }
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: 'AdresseMailEmployeeTest@test.test'}))
+    let newbill = new NewBill({document:document.body,onNavigate:onNavigate,store:mockStore,localStorage:window.localStorage});
+    let inputFile = document.body.querySelector(`input[data-testid="file"]`)
+
+    test("Then if file type is ok", async () => {
+      const file = new File(['test'], 'chucknorris.jpg', { type: 'image/jpg' })
+      await fireEvent.change(inputFile, {target: { files: [file] }} );
+      expect(typeok.includes(inputFile.files[0].type)).toBe(true);
+    })
+
+    test("Then if file type is not ok", async () => {
+      const file = new File(['test'], 'chucknorris.jpzzzg', { type: 'image/jpzzzg' })
+      await fireEvent.change(inputFile, {target: { files: [file] }} );
+      expect(typeok.includes(inputFile.files[0].type)).not.toBe(true);
+    })
+
+    test("Then if multiple file blocked", async () => {
+      const file = new File(['test'], 'chucknorris.jpg', { type: 'image/jpg' })
+      await fireEvent.change(inputFile, {target: { files: [file,file] }} );
+      expect(inputFile.files.length).not.toBe(1);
+    })
+
+    test("Then if jpeg img is ok", () => {
+      let img = {name:"Photodevacance",type:"image/jpeg"};
+      expect(typeok.includes(img.type)).toBe(true);
+    })
+
+    test("Then if png img is ok", () => {
+      let img = {name:"Photodevacance",type:"image/png"};
+      expect(typeok.includes(img.type)).toBe(true);
+    })
+
+    test("Then if type of img is not ok", () => {
+      let img = {name:"Photodevacance",type:"image/bmp"};
+      expect(typeok.includes(img.type)).not.toBe(true);
+    })
+    //Generation form
+    let data = {
+      expense_name : "TEST Vol Paris Londres",
+      datepicker : "2000-01-01",
+      amount : "1",
+      vat : 70,
+      pct : 20,
+      commentary : "TEST"
+    }
+    screen.getByTestId("expense-name").value = data.expense_name;
+      screen.getByTestId("datepicker").value = data.datepicker;
+      screen.getByTestId("amount").value = data.amount;
+      screen.getByTestId("vat").value = data.vat;
+      screen.getByTestId("pct").value = data.pct;
+      screen.getByTestId("commentary").value = data.commentary;
+
+    test("Then send form with no picture", async () => {
+            
+      await fireEvent.change(inputFile, {target: { files: [] }} )
+      await fireEvent.submit(screen.getByTestId("form-new-bill"));
+      
+      expect(newbill.succes).not.toBe(true);
+    })
+    
+    test("Then send form", async () => {
+      const file = new File(['test'], 'chucknorris.jpg', { type: 'image/jpg' })
+      await fireEvent.change(inputFile, {target: { files: [file] }} )
+      
+      await fireEvent.submit(screen.getByTestId("form-new-bill"));
+      
+      expect(newbill.succes).toBe(true);
     })
   })
 })
--- frontend/src/containers/NewBill.js
+++ frontend/src/containers/NewBill.js
@@ -3,7 +3,6 @@ import Logout from "./Logout.js"
 
 export default class NewBill {
   constructor({ document, onNavigate, store, localStorage }) {
-    console.log("Creation")
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
@@ -14,48 +13,59 @@ export default class NewBill {
     this.fileUrl = null
     this.fileName = null
     this.billId = null
+    this.succes = false
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
     e.preventDefault()
     let typeok = ["image/jpg","image/jpeg","image/png"];
-    let file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    if(!typeok.includes(file.type)){
-      alert("Mauvais format");
-      this.fileUrl = null
-      this.fileName = null
-    }else{
-      const filePath = e.target.value.split(/\\/g)
-      const fileName = filePath[filePath.length-1]
-      const formData = new FormData()
-      const email = JSON.parse(localStorage.getItem("user")).email
-      formData.append('file', file)
-      formData.append('email', email)
+    if(this.document.querySelector(`input[data-testid="file"]`).files.length === 1){
+      let file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+        if(!typeok.includes(file.type)){
+          //alert("Mauvais format");
+          this.fileUrl = null
+          this.fileName = null
+        }else{
+          const filePath = this.document.querySelector(`input[data-testid="file"]`).files[0].name.split(/\\/g)
+          const fileName = filePath[filePath.length-1]
+          const formData = new FormData()
+          const email = JSON.parse(localStorage.getItem("user")).email
+          formData.append('file', file)
+          formData.append('email', email)
 
-      this.store
-        .bills()
-        .create({
-          data: formData,
-          headers: {
-            noContentType: true
-          }
-        })
-        .then(({fileUrl, key}) => {
-          console.log(fileUrl)
-          this.billId = key
-          this.fileUrl = fileUrl
-          this.fileName = fileName
-        }).catch(error => console.error(error))
+          this.store
+            .bills()
+            .create({
+              data: formData,
+              headers: {
+                noContentType: true
+              }
+            })
+            .then(({fileUrl,  key}) => {
+              this.billId = key
+              this.fileUrl = fileUrl
+              this.fileName = fileName
+              
+            }).catch(error => console.error(error))
+        
+        }
+    }else{
+      return Error
     }
   }
+
   handleSubmit = e => {
     e.preventDefault()
     if(this.fileUrl === null && this.fileName === null){
-      alert("Problème de format avec votre image");
+      //alert("Problème avec votre image");
       //Ajouter verif autre input
     }else{
-      console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-      const email = JSON.parse(localStorage.getItem("user")).email
+      let user;
+      user = JSON.parse(localStorage.getItem('user'))
+      if (typeof user === 'string') {
+        user = JSON.parse(user)
+      }
+      const email = user.email
       const bill = {
         email,
         type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
@@ -69,6 +79,8 @@ export default class NewBill {
         fileName: this.fileName,
         status: 'pending'
       }
+      console.log(bill)
+      this.succes = true
       this.updateBill(bill)
       this.onNavigate(ROUTES_PATH['Bills'])
     }
